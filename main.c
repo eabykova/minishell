3976f29e1fb4156199a6d980aab22625b8537227
Miki Michelina <mmicheli@oa-c3.msk.21-school.ru>
2022-05-30 17:42:13 +0300

    using pipe

--- main.c
+++ main.c
@@ -6,22 +6,24 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 21:50:05 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/05/29 20:51:00 by mmicheli         ###   ########.fr       */
+/*   Updated: 2022/05/30 16:48:13 by mmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "includes/minishell.h"
 
-int	main(void)
+int	main(int argc, char **argv, char **envp)
 {
 	t_list	**cmd_list;
 
 	printf("Hello, I'm minishell!\n");
-	cmd_init(cmd_list);
-	while (cmd_list)
-	{
-		ft_printf("%d\n", (*cmd_list)->content);
-		cmd_list++;
-	}
-	executor();
+//	cmd_init(cmd_list);
+//	while (cmd_list)
+//	{
+//		ft_printf("%d\n", (*cmd_list)->content);
+//		cmd_list++;
+//	}
+//	infile_to_cmd1();
+//	cmd1_to_cmd2(envp);
+	cmd2_to_out();
 }
