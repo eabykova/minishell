5889421b9b60d651d9649344fee8f8a55e0ae9d4
Miki Michelina <mmicheli@mi-a4.msk.21-school.ru>
2022-06-01 15:59:11 +0300

    cerate parent child

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 21:50:05 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/05/30 17:52:36 by mmicheli         ###   ########.fr       */
+/*   Updated: 2022/06/01 14:54:02 by mmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,26 @@
 
 int	main(int argc, char **argv, char **envp)
 {
-	printf("Hello, I'm minishell!\n");
-	cmd_init();
-	executor();
+	int	in_fil;
+	int	out_fil;
+
+	if (argc == 5)
+	{
+		printf("Hello, I'm minishell!\n");
+		in_fil = open(argv[1], O_RDONLY);
+		if (in_fil < 0)
+			ft_kraft_error("Error in in_fil opening.\n", 1);
+		out_fil = open(argv[4], O_CREAT | O_WRONLY | O_TRUNC, 0644);
+		if (out_fil < 0)
+			ft_kraft_error("Error in out_fil opening.\n", 1);
+		cmd_init();
+		executor();
+	}
+	else
+	{
+		if (argc < 5)
+			printf("Too few arguments. Try again.\n");
+		else
+			printf ("Too much arguments. Try again.\n");
+	}
 }
--- src/executor.c
+++ src/executor.c
@@ -6,26 +6,34 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 14:36:29 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/05/30 18:18:40 by mmicheli         ###   ########.fr       */
+/*   Updated: 2022/06/01 14:39:19 by mmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
+static void	child_proc(void)
+{
+	ft_printf("Into child proc\n");
+}
+
+static void	parent_proc(void)
+{
+	ft_printf("Into parent proc\n");
+}
+
 void	executor(void)
 {
-	int	pid;
+	pid_t	pid;
+	int		end[2];
 
 	ft_printf("Into executor\n");
+	pipe(end);
 	pid = fork();
 	if (pid == -1)
 		ft_kraft_error("Error in fork\n", EXIT_FAILURE);
 	if (pid == 0)
-	{
-		ft_printf("Into child proc, pid = %d\n", pid);
-	}
+		child_proc();
 	else
-	{
-		ft_printf("Into parent proc, pid = %d\n", pid);
-	}
+		parent_proc();
 }
