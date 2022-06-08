6a829702b208e4f4b7182356c9c23efb028a8ed4
lykalon <lykalon@student.21-school.ru>
2022-06-08 20:20:47 +0300

    revert

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 12:45:46 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/05/30 17:44:43 by mmicheli         ###   ########.fr       */
+/*   Updated: 2022/06/04 15:28:30 by lykalon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,40 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../lib/libft.h"
+
+typedef struct s_env
+{
+	char			*key;
+	void			*value;
+	struct s_env	*next;
+}			t_env;
+
+typedef struct s_command
+{
+	char				*token;
+	struct s_command	*next;
+}			t_command;
+
+typedef struct s_pipe
+{
+	t_command		*command;
+	struct s_pipe	*next;
+}			t_pipe;
+
+typedef struct s_redir
+{
+	char	*in;
+	char	*out;
+	char	*heredoc;
+	char	*another_one;
+}			t_redir;
+
+typedef struct s_data
+{
+	t_env	*env;
+	t_pipe	*pipe;
+	t_redir	*redir;
+}			t_data;
 
 void	executor(void);
 void	cmd_init(t_list **list);
--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,46 @@
 /*   By: lykalon <lykalon@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/29 20:23:00 by lykalon           #+#    #+#             */
-/*   Updated: 2022/05/30 17:45:50 by mmicheli         ###   ########.fr       */
+/*   Updated: 2022/06/04 16:08:34 by lykalon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../includes/minishell.h"
+
+t_env	*extract_env(char **envp)
+{
+	int		i;
+	int		j;
+	t_env	*env;
+	t_env	*new;
+	char	**buff;
+
+	i = 0;
+	env = NULL;
+	while (envp[i])
+	{
+		j = 0;
+		buff = ft_split(envp[i], '=');
+		new = ft_env_new(buff);
+		while (buff[j])
+		{
+			free(buff[j]);
+			++j;
+		}
+		free(buff);
+		ft_env_pushback(&env, new);
+		++i;
+	}
+	return (env);
+}
+
+t_data	*parser(char *line, char **envp)
+{
+	t_data	*data;
+
+	data = malloc(sizeof(t_data));
+	data->env = extract_env(envp);
+	data->redir = extract_redirects();
+	data->pipe = extract_pipes();
+	return (data);
+}
