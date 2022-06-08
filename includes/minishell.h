4717a03ab51755bb435c56605480496aa838cfa6
lykalon <lykalon@student.21-school.ru>
2022-06-08 20:50:16 +0300

    123

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 12:45:46 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/06/04 15:28:30 by lykalon          ###   ########.fr       */
+/*   Updated: 2022/06/08 20:48:35 by lykalon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,26 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 # include "../lib/libft.h"
+
+# define ERROR_INFILE "Error in infile opening.\n"
+# define ERROR_OUTFILE "Error in outfile opening.\n"
+# define TOO_FEW_ARGS "Too few arguments. Try again.\n"
+# define TOO_MUCH_ARGS "Too much arguments. Try again.\n"
+# define PIPE_ERROR "Error in pipe.\n"
+# define FORK_ERROR "Error in fork.\n"
+# define SPLIT_ERROR "Error in ft_split.\n"
+
+typedef struct s_ppx
+{
+	int		in_fil;
+	int		out_fil;
+	int		end[2];
+	pid_t	pid_1;
+	pid_t	pid_2;
+	char	**path;
+	char	**cmd_args;
+	char	*cmd;
+}				t_ppx;
 
 typedef struct s_env
 {
@@ -54,7 +74,10 @@ typedef struct s_data
 	t_redir	*redir;
 }			t_data;
 
-void	executor(void);
-void	cmd_init(t_list **list);
+void	executor(t_ppx *pipex,	char **argv, char **envp);
+void	env_init(t_ppx *pipex, char **envp);
+//void	cmd_init(t_ppx *pipex, char *cmd);
+
+void	perror_exit(char *error);
 
 #endif
--- src/executor.c
+++ src/executor.c
@@ -6,13 +6,62 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 14:36:29 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/06/04 12:34:33 by lykalon          ###   ########.fr       */
+/*   Updated: 2022/06/08 20:48:35 by lykalon          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-void	executor(void)
+static void	first_child(t_ppx *pipex, char *cmd, char **envp)
 {
+	pipex->cmd_args = ft_split(cmd, ' ');
+	printf("Into first_child proc\n");
+	dup2(pipex->end[1], STDOUT_FILENO);
+	close(pipex->end[0]);
+	dup2(pipex->in_fil, STDIN_FILENO);
+	close(pipex->in_fil);
+	execve("/bin/ls", pipex->cmd_args, envp);
+}
 
+static void	second_child(t_ppx *pipex, char *cmd, char **envp)
+{
+	pipex->cmd_args = ft_split(cmd, ' ');
+	printf("Into second_child proc\n");
+	dup2(pipex->end[0], STDIN_FILENO);
+	close(pipex->end[1]);
+	dup2(pipex->out_fil, STDOUT_FILENO);
+	execve("/bin/cat", pipex->cmd_args, envp);
+}
+
+static void	parent_proc(t_ppx *pipex, char *cmd_2)
+{
+	int	status;
+
+	waitpid(-1, &status, 0);
+	printf("Into parent proc\n");
+//	dup2();
+}
+
+void	executor(t_ppx *pipex,	char **argv, char **envp)
+{
+	printf("Into executor\n");
+	if (pipe(pipex->end) < 0)
+		perror_exit(PIPE_ERROR);
+	pipex->pid_1 = fork();
+	if (pipex->pid_1 == -1)
+		perror_exit(FORK_ERROR);
+	if (pipex->pid_1 == 0)
+	{
+		printf("argv[2] : %s\n", argv[2]);
+		first_child(pipex, argv[2], envp);
+	}
+	pipex->pid_2 = fork();
+	if (pipex->pid_2 == -1)
+		perror_exit(FORK_ERROR);
+	if (pipex->pid_2 == 0)
+	{
+		printf("argv[3] : %s\n", argv[3]);
+		second_child(pipex, argv[3], envp);
+	}//	else
+//		parent_proc(pipex, cmd_2);
 }
