60fffc8ecb8e2a7d3935c0d5d9cce0d556b27221
Miki Michelina <mmicheli@at-m4.msk.21-school.ru>
2022-06-08 20:29:38 +0300

    revert

--- src/executor.c
+++ src/executor.c
@@ -6,42 +6,31 @@
 /*   By: mmicheli <mmicheli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/26 14:36:29 by mmicheli          #+#    #+#             */
-/*   Updated: 2022/06/08 13:36:20 by mmicheli         ###   ########.fr       */
+/*   Updated: 2022/06/08 20:29:10 by mmicheli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static void	first_child(t_ppx *pipex, char *cmd_1, char **envp)
+static void	first_child(t_ppx *pipex, char *cmd, char **envp)
 {
-	char	*fl[2];
-
+	pipex->cmd_args = ft_split(cmd, ' ');
 	printf("Into first_child proc\n");
-	fl[0] = malloc(3);
-	fl[0] = "ls\0";
-	fl[1] = NULL;
 	dup2(pipex->end[1], STDOUT_FILENO);
 	close(pipex->end[0]);
 	dup2(pipex->in_fil, STDIN_FILENO);
 	close(pipex->in_fil);
-	execve("/bin/ls", fl, envp);
-	printf("Will not see in terminal\n");
+	execve("/bin/ls", pipex->cmd_args, envp);
 }
 
-static void	second_child(t_ppx *pipex, char *cmd_1, char **envp)
+static void	second_child(t_ppx *pipex, char *cmd, char **envp)
 {
-	char	*fl[3];
-
+	pipex->cmd_args = ft_split(cmd, ' ');
 	printf("Into second_child proc\n");
-	fl[0] = malloc(3);
-	fl[0] = "ls\0";
-	fl[1] = malloc(4);
-	fl[1] = "-la\0";
-	fl[2] = NULL;
 	dup2(pipex->end[0], STDIN_FILENO);
 	close(pipex->end[1]);
 	dup2(pipex->out_fil, STDOUT_FILENO);
-	execve("/bin/ls", fl, envp);
+	execve("/bin/cat", pipex->cmd_args, envp);
 }
 
 static void	parent_proc(t_ppx *pipex, char *cmd_2)
@@ -53,11 +42,8 @@ static void	parent_proc(t_ppx *pipex, char *cmd_2)
 //	dup2();
 }
 
-void	executor(t_ppx *pipex, char **envp)
+void	executor(t_ppx *pipex,	char **argv, char **envp)
 {
-	char	cmd_1[5] = "ls -a";
-	char	cmd_2[5] = "wc -l";
-
 	printf("Into executor\n");
 	if (pipe(pipex->end) < 0)
 		perror_exit(PIPE_ERROR);
@@ -65,12 +51,17 @@ void	executor(t_ppx *pipex, char **envp)
 	if (pipex->pid_1 == -1)
 		perror_exit(FORK_ERROR);
 	if (pipex->pid_1 == 0)
-		first_child(pipex, cmd_1, envp);
+	{
+		printf("argv[2] : %s\n", argv[2]);
+		first_child(pipex, argv[2], envp);
+	}
 	pipex->pid_2 = fork();
 	if (pipex->pid_2 == -1)
 		perror_exit(FORK_ERROR);
 	if (pipex->pid_2 == 0)
-		second_child(pipex, cmd_1, envp);
-	else
-		parent_proc(pipex, cmd_2);
+	{
+		printf("argv[3] : %s\n", argv[3]);
+		second_child(pipex, argv[3], envp);
+	}//	else
+//		parent_proc(pipex, cmd_2);
 }
